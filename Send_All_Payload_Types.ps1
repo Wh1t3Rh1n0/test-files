67a4b06cdfef74ef0d8d48559f32a80e5afaf410
Wh1t3Rh1n0 <Wh1t3Rh1n0@users.noreply.github.com>
2025-05-06 23:45:21 -0400

    Fixes

--- Send_All_Payload_Types.ps1
+++ Send_All_Payload_Types.ps1
@@ -1,4 +1,9 @@
-﻿function Set-Credential {
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.Net")
+[System.Reflection.Assembly]::LoadWithPartialName("System.Net.Mail") 
+[System.Reflection.Assembly]::LoadWithPartialName("System.Net.Mail.MailMessage")
+
+
+function Set-Credential {
     param (
         [Parameter(Mandatory = $true)] [string]$user_id,
         [Parameter(Mandatory = $true)] [string]$password
@@ -9,6 +14,68 @@
         (ConvertTo-SecureString $password -AsPlainText -Force))
     )
 }
+
+
+function Send-MyMailMessage {
+    param (
+        [Parameter(Mandatory = $true)] [string]$From,
+        [Parameter(Mandatory = $true)] [string[]]$To,
+        [string[]]$Cc,
+        [string[]]$Bcc,
+        [Parameter(Mandatory = $true)] [string]$Subject,
+        [Parameter(Mandatory = $true)] [string]$Body,
+        [Parameter(Mandatory = $true)] [string]$SmtpServer,
+        [int]$Port = 587,
+        [Parameter(Mandatory = $true)] [pscredential]$Credential,
+        [switch]$UseSsl,
+        [string[]]$Attachments,
+        [switch]$BodyAsHtml
+    )
+
+    #Add-Type -AssemblyName System.Net.Mail
+    #Add-Type -AssemblyName System.Net
+
+    $mail = New-Object System.Net.Mail.MailMessage
+    $mail.From = New-Object System.Net.Mail.MailAddress($From)
+
+    # Add recipients
+    foreach ($recipient in $To)  { if ($recipient) { $mail.To.Add($recipient) } }
+    foreach ($recipient in $Cc)  { if ($recipient) { $mail.CC.Add($recipient) } }
+    foreach ($recipient in $Bcc) { if ($recipient) { $mail.Bcc.Add($recipient) } }
+
+    $mail.Subject = $Subject
+    $mail.Body = $Body
+    $mail.IsBodyHtml = $BodyAsHtml.IsPresent
+
+    # Add attachments
+    if ($Attachments) {
+        foreach ($path in $Attachments) {
+            if (Test-Path $path) {
+                $attachment = New-Object System.Net.Mail.Attachment($path)
+                $mail.Attachments.Add($attachment)
+            } else {
+                Write-Error "Attachment not found: $path"
+                return
+            }
+        }
+    }
+
+    $smtp = New-Object System.Net.Mail.SmtpClient($SmtpServer, $Port)
+    $smtp.Credentials = New-Object System.Net.NetworkCredential(
+        $Credential.UserName,
+        $Credential.GetNetworkCredential().Password
+    )
+    $smtp.EnableSsl = $UseSsl.IsPresent
+
+    try {
+        $smtp.Send($mail)
+    } catch {
+        Write-Error "Failed to send email: $_"
+    } finally {
+        $mail.Dispose()
+    }
+}
+
 
 
 function Send-Messages {
@@ -52,7 +119,7 @@ function Send-Messages {
 
         if ( $type -eq "attachment" ) {
             $MailParams.Attachments = ( $($attachments_folder) + $_.Name )
-            #echo $MailParams.Attachments
+            echo $MailParams.Attachments
         } elseif ( $type -eq "message" ) {
             $body = $_.FullName
         } else {
@@ -70,10 +137,20 @@ function Send-Messages {
         }
 
 
-        echo ( "Sending: '" + $MailParams.Subject + "'..." )
+        $output = ( ( get-date -Format "yyyy-MM-dd HH:mm K> " ) + "SENDING: '" + $MailParams.Subject + "'..." ) 
+        echo $output
+        if ( $log_file ) { echo $output | add-content $log_file }
     
-        Send-MailMessage @MailParams
+        if ( $method -eq 1 ) { 
+            Send-MailMessage @MailParams
+        } elseif ($method -eq 2) { 
+            Send-MyMailMessage @MailParams 
+        } else {
+            echo "ERROR: Method must be set to 1 or 2. Quitting."
+            exit
+        }
 
+        echo "...Sleeping $sleep_interval seconds..."
         start-sleep $sleep_interval
     }
 
@@ -95,13 +172,19 @@ function Invoke-EmailTest {
 
         [string]$attachments_folder = "$PWD\Attachments\",
         [string]$messages_folder = "$PWD\Messages\",
-        [string]$sleep_interval = 5
+        [string]$sleep_interval = 60,
+
+        [string]$log_file = "./EmailTest.log",
+
+        [string]$method = 2
     )
 
     $global:email_counter = 0
-
+    
+    echo ""
     Send-Messages -type "message" -folder $messages_folder
     Send-Messages -type "attachment" -folder $attachments_folder
+    
 
     echo "ALL DONE!"
 }
