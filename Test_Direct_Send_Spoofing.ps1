121747c6d03ae547b040300d7ff74bf73a58a2c1
Wh1t3Rh1n0 <Wh1t3Rh1n0@users.noreply.github.com>
2025-05-06 22:11:10 -0400

    Fixed sleep interval

--- Test_Direct_Send_Spoofing.ps1
+++ Test_Direct_Send_Spoofing.ps1
@@ -17,13 +17,17 @@
     echo "Sending: '$subject'..."
     Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_valid_mailbox -To $to
 
+    start-sleep $sleep_interval
 
     $subject = "DS Test 2: Sent from non-existent mailbox"
     echo "Sending: '$subject'..."
     Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_invalid_mailbox -To $to
 
+    start-sleep $sleep_interval
 
     $subject = "DS Test 3: Sent from external domain"
     echo "Sending: '$subject'..."
     Send-MailMessage -SmtpServer $smart_host -Subject $subject -Body $body -BodyAsHtml -From $from_external_domain -To $to
+    
+    echo "All done!"
 }
